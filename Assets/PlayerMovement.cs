c74ec1527db8fba9590ee11cae1e697d9a57095c
Carlitos <CarloosAF@hotmail.es>
2020-03-21 20:03:07 +0100

    un poco de player movement fisicas y animacion

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpSpeed;
+    public float moveSpeed;
+    public bool moving = false;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }else if (Input.GetKey(KeyCode.A)){
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }else if  (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+        }
+    }
+}
