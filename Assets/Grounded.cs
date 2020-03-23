cb0118694c31381fc13535f9a6a944e5e399a94a
Carlitos <CarloosAF@hotmail.es>
2020-03-23 22:56:21 +0100

    Animacion y movimiento casis completo, solo falta girar el sprite

---
+++ Assets/Grounded.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = gameObject.transform.parent.gameObject;    
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            player.GetComponent<PlayerMovement>().grounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            player.GetComponent<PlayerMovement>().grounded = false;
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,18 +5,51 @@ public class PlayerMovement : MonoBehaviour
 {
     public float jumpSpeed;
     public float moveSpeed;
-    public bool moving = false;
+    public bool grounded = false;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    Animator anim;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))    // Derecha
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }else if (Input.GetKey(KeyCode.A)){
-
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }else if  (Input.GetKeyDown(KeyCode.W))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
         }
+        if (Input.GetKey(KeyCode.A)){  // Izquierda 
+
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        if  (Input.GetKeyDown(KeyCode.W) && grounded )  //Salto
+        {
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            jumpTimeCounter = jumpTime;
+        }
+        if (Input.GetKey(KeyCode.W) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isJumping = false;
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+
     }
 }
